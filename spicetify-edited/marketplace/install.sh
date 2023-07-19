fe7d787d1f28a3239e2236b403aac0f038b94980
n1ghtdev <135556230+n1d3v@users.noreply.github.com>
2023-07-19 08:10:14 -0700

    Update install.sh

--- spicetify-edited/marketplace/install.sh
+++ spicetify-edited/marketplace/install.sh
@@ -33,7 +33,7 @@ fi
 TAR_FILE="$INSTALL_DIR/marketplace-dist.zip"
 
 echo "DOWNLOADING $download_uri"
-curl --fail --location --progress-bar --output -k "$TAR_FILE" "$download_uri"
+curl --fail --location --progress-bar --output --insecure "$TAR_FILE" "$download_uri"
 cd "$INSTALL_DIR"
 
 echo "EXTRACTING"

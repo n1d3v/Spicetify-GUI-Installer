76bae5592e9a57fb32b23dada8c04789a094fb5b
n1ghtdev <135556230+n1d3v@users.noreply.github.com>
2023-07-19 08:09:04 -0700

    Update install.sh

--- spicetify-edited/marketplace/install.sh
+++ spicetify-edited/marketplace/install.sh
@@ -4,7 +4,6 @@
 
 set -e
 
-   
 # download uri
 releases_uri=https://github.com/spicetify/spicetify-marketplace/releases
 if [ $# -gt 0 ]; then
@@ -21,14 +20,14 @@ tag=${tag#v}
 echo "FETCHING Version $tag"
 
 download_uri=$releases_uri/download/v$tag/spicetify-marketplace.zip
-    default_color_uri="https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/color.ini"
+default_color_uri="https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/color.ini"
 
 SPICETIFY_CONFIG_DIR="${SPICETIFY_CONFIG:-$HOME/.config/spicetify}"
 INSTALL_DIR="$SPICETIFY_CONFIG_DIR/CustomApps"
 
 if [ ! -d "$INSTALL_DIR" ]; then
-    echo "MAKING FOLDER  $INSTALL_DIR";
-    mkdir -p "$INSTALL_DIR"
+	echo "MAKING FOLDER  $INSTALL_DIR";
+	mkdir -p "$INSTALL_DIR"
 fi
 
 TAR_FILE="$INSTALL_DIR/marketplace-dist.zip"
@@ -57,23 +56,23 @@ spicetify config replace_colors 1
 
 current_theme=$(spicetify config current_theme)
 if [ ${#current_theme} -le 3 ]; then
-    echo "No theme selected, using placeholder theme"
-    if [ ! -d "$SPICETIFY_CONFIG_DIR/Themes/marketplace" ]; then
-        echo "MAKING FOLDER  $SPICETIFY_CONFIG_DIR/Themes/marketplace";
-        mkdir -p "$SPICETIFY_CONFIG_DIR/Themes/marketplace"
-    fi
-    curl --fail --location --progress-bar --output -k "$SPICETIFY_CONFIG_DIR/Themes/marketplace/color.ini" "$default_color_uri"
-    spicetify config current_theme marketplace;
+	echo "No theme selected, using placeholder theme"
+	if [ ! -d "$SPICETIFY_CONFIG_DIR/Themes/marketplace" ]; then
+		echo "MAKING FOLDER  $SPICETIFY_CONFIG_DIR/Themes/marketplace";
+		mkdir -p "$SPICETIFY_CONFIG_DIR/Themes/marketplace"
+	fi
+	curl --fail --location --progress-bar --output -k "$SPICETIFY_CONFIG_DIR/Themes/marketplace/color.ini" "$default_color_uri"
+	spicetify config current_theme marketplace;
 fi
 
 if spicetify config custom_apps marketplace ; then
-    echo "Added to config!"
-    echo "APPLYING"
-    spicetify apply
+	echo "Added to config!"
+	echo "APPLYING"
+	spicetify apply
 else
-    echo "Command failed"
-    echo "Please run \`spicetify config custom_apps marketplace\` manually "
-    echo "Next run \`spicetify apply\`"
+	echo "Command failed"
+	echo "Please run \`spicetify config custom_apps marketplace\` manually "
+	echo "Next run \`spicetify apply\`"
 fi
 
 echo "CLEANING UP"
